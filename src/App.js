5785b9dfcc94fbeae912d40a45d00f295b1502d5
Aleece <aleeceburgess@gmail.com>
2020-01-10 09:45:39 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+import SisterCities from "./components/SisterCities/SisterCitiesContainer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <SisterCities />
+    </div>
+  );
+}
---
+++ src/components/SisterCities/SisterCities.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class SisterCities extends React.Component {
+  render = () => {
+    return <h1>Hullo</h1>;
+  };
+}
